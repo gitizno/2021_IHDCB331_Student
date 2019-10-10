b281d312957d3bd91032ba246f832009bcf311ad
jortizve <james.ortizvega@unamur.be>
2019-10-10 13:55:37 +0200

    init repo

---
+++ src/main/java/be/unamur/info/algo2/Main.java
@@ -0,0 +1,63 @@
+package be.unamur.info.algo2;
+
+public class Main {
+
+
+    public static String[] problem_1_naive(String s_file) {
+        System.out.println("===");
+        System.out.println(s_file);
+        System.out.println("===");
+        String[] s_tmp = {"Goku", "Piccolo", null, "Gohan"};
+        //String[] s_tmp = null;
+        return s_tmp;
+    }
+
+
+    public static String[] problem_1(String s_file) {
+        System.out.println("===");
+        System.out.println(s_file);
+        System.out.println("===");
+        String[] s_tmp = {"Goku", "Piccolo", null, "Gohan"};
+        //String[] s_tmp = null;
+        return s_tmp;
+    }
+
+    public static String[] problem_2_naive(String s_file) {
+        System.out.println("====");
+        System.out.println(s_file);
+        System.out.println("====");
+        String[] s_tmp = {"8", "1", "3", "0"};
+        //String[] s_tmp = null;
+        return s_tmp;
+    }
+
+    public static String[] problem_2(String s_file) {
+        System.out.println("====");
+        System.out.println(s_file);
+        System.out.println("====");
+        String[] s_tmp = {"8", "1", "3", "0"};
+        //String[] s_tmp = null;
+        return s_tmp;
+    }
+
+    public static String[] problem_3_naive(String s_file) {
+        System.out.println("===");
+        System.out.println(s_file);
+        System.out.println("===");
+        String[] s_tmp = {"1", "0", "1", "1"};
+        //String[] s_tmp = null;
+        return s_tmp;
+    }
+
+
+    public static String[] problem_3(String s_file) {
+        System.out.println("===");
+        System.out.println(s_file);
+        System.out.println("===");
+        String[] s_tmp = {"1", "0", "1", "1"};
+        //String[] s_tmp = null;
+        return s_tmp;
+    }
+
+
+}
