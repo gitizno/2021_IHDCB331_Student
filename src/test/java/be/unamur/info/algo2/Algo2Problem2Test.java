08a27adbccea1c357f0d90937556f60e981e534a
jortizve <james.ortizvega@unamur.be>
2019-10-10 16:38:20 +0200

    modif fichiers

--- src/test/java/be/unamur/info/algo2/Algo2Problem2Test.java
+++ src/test/java/be/unamur/info/algo2/Algo2Problem2Test.java
@@ -41,7 +41,7 @@ public class Algo2Problem2Test {
         Main main = new Main();
         String input = "src/test/resources/problem2/ProgrammationDynamique_3.2.txt";
         String[] result = main.problem_2(getFileText(input));
-        String[] s_result = {"10", "-1", "6, "-1"};
+        String[] s_result = {"10", "-1", "6", "-1"};
         assertThat("Testing size array", result.length == 4);
         assertThat("Testing value[0]", result[0].equals(s_result[0]));
         assertThat("Testing value[1]", result[1].equals(s_result[1]));
